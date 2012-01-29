20c57e04e89a5def40ea1bf635597d6a937e072d
Wentao Han <wentao.han@gmail.com>
2012-01-29 21:29:59 +0800

    Import source code from Google Code.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+setup(
+	name = "libgmbox",
+	version = "1.0",
+	packages = ["libgmbox"],
+	author = "muzuiget",
+	author_email = "muzuiget@gmail.com",
+	description = "谷歌音乐解析库",
+	license = "GPL",
+)
